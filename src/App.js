0a9072a7bc0cbbce747b01b475bdd9676af27acc
kareem nour <kimzcodes@gmail.com>
2021-11-21 16:32:59 +0200

    completed examle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import Button from './components/Button';
-import ButtonGroup from './components/ButtonGroup';
+import List from './components/List';
+import ShapOne from './components/ShapOne';
+import ShapTwo from './components/ShapTwo';
 
 function App() {
-  // const userData = [
-  //   { id: 1, name: 'kareem', email: 'kareem@email.com', age: 30 },
-  //   { id: 2, name: 'amr', email: 'amr@email.com', age: 30 },
-  //   { id: 3, name: 'wafaa', email: 'wafaa@email.com', age: 32 },
-  //   { id: 4, name: 'safaa', email: 'safaa@email.com', age: 33 },
-  // ];
-  const callBackFn = (title) => {
-    console.log(title);
-  };
+  const userData = [
+    { id: 1, name: 'kareem', email: 'kareem@email.com', age: 30 },
+    { id: 2, name: 'amr', email: 'amr@email.com', age: 30 },
+    { id: 3, name: 'wafaa', email: 'wafaa@email.com', age: 32 },
+    { id: 4, name: 'safaa', email: 'safaa@email.com', age: 33 },
+  ];
+
+  const userData2 = [
+    { id: 1, name: 'aly', email: 'kareem@email.com', age: 30 },
+    { id: 2, name: 'hesham', email: 'amr@email.com', age: 30 },
+  ];
 
   return (
     <div className='App'>
-      <Button callBackFn={callBackFn}>Single</Button>
-
-      <ButtonGroup>
-        <Button callBackFn={callBackFn}>First</Button>
-        <Button callBackFn={callBackFn}>Second</Button>
-      </ButtonGroup>
+      <List items={userData}>
+        <ShapOne />
+      </List>
+      <List items={userData2}>
+        <ShapTwo />
+      </List>
     </div>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,18 +1,11 @@
 import React from 'react';
 
 const List = ({ children, items }) => {
-  const itemHandler = items.map((item) =>
-    React.cloneElement(
-      children,
-      {
-        key: item.id,
-        ...item,
-      },
-      'wrapper'
-    )
+  const dataHandler = items.map((item) =>
+    React.cloneElement(children, { key: item.id, item })
   );
 
-  return <div className='list'>{itemHandler}</div>;
+  return <div className='list'>{dataHandler}</div>;
 };
 
 export default List;
